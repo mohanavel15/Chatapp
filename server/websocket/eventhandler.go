37ca6f7c7793900dea66f37287712a5c0d8ed6e8
Mohan <mohanavel15@protonmail.com>
2022-09-29 13:21:52 +0530

    added mutex protection for event handler

--- server/websocket/eventhandler.go
+++ server/websocket/eventhandler.go
@@ -1,20 +1,28 @@
 package websocket
 
+import "sync"
+
 type Function func(*Context)
 
 type EventHandler struct {
+	mutex      sync.Mutex
 	HandleFunc map[string]Function
 }
 
 func (eh *EventHandler) Handle(ctx Context) {
+	eh.mutex.Lock()
 	if handler, ok := eh.HandleFunc[ctx.Event]; ok {
+		eh.mutex.Unlock()
 		handler(&ctx)
 	}
+	eh.mutex.Unlock()
 }
 
 func (eh *EventHandler) Add(event string, handler Function) {
+	eh.mutex.Lock()
 	if eh.HandleFunc == nil {
 		eh.HandleFunc = make(map[string]Function)
 	}
 	eh.HandleFunc[event] = handler
+	eh.mutex.Unlock()
 }
