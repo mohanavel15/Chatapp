446544d1bacfd08ef122c81151460bfcb1e0fdcf
Mohan <mohanavel15@protonmail.com>
2023-02-22 13:00:13 +0530

    re-did recipient component

--- web/src/components/channel/Recipient.tsx
+++ web/src/components/channel/Recipient.tsx
@@ -1,38 +1,19 @@
-import React from 'react'
 import { UserOBJ } from '../../models/models';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle, faDotCircle, faCircleMinus, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { setDefaultAvatar } from '../../utils/errorhandle';
 import { FaCrown } from "react-icons/fa";
+import { RxDot, RxDotFilled } from "react-icons/rx"
 
 export default function Recipient({ user, isOwner }: {user: UserOBJ, isOwner: boolean }) {
-    let style: React.CSSProperties
-    let icon: IconDefinition
-    if (user.status === 1) {
-        style = {
-            color: "lime"
-        }
-        icon = faCircle
-    } else if (user.status === 2) {
-        style = {
-            color: "red"
-        }
-        icon = faCircleMinus
-    } else {
-        style = {
-            color: "grey"
-        }
-        icon = faDotCircle
-    }
-
     return (
         <div className='h-12 flex items-center rounded hover:bg-zinc-900 cursor-pointer'>
-            <div className='avatar-container'>
-            <img src={user.avatar} onError={setDefaultAvatar} alt={"Icon"} />
-            <FontAwesomeIcon className='status_icon' style={style} icon={icon} />
+            <div className='relative h-10 w-10 mx-4'>
+            <img className='rounded-xl' src={user.avatar} onError={setDefaultAvatar} alt={"Icon"} />
+            <div className='absolute right-0 bg-black rounded-full bottom-0'>
+                { user.status === 1 ? <RxDotFilled size={20} className="text-green-600" /> : <RxDot size={20} className="text-gray-400" /> }
+            </div>
             </div>
             <p>{user.username}</p>
-            { isOwner && <div className='owner-icon'><FaCrown /></div> }
+            { isOwner && <div className='text-yellow-500 mx-2'><FaCrown /></div> }
         </div>
     )
 }
